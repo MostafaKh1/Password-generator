4a341c93aaacee899203d33a11116c8a2aab9d7f
MostafaKh1 <chiiit1754@gmail.com>
2023-04-25 15:32:55 +0200

    fix input

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -4,7 +4,12 @@ export default function Footer() {
   return (
     <footer className="w-full   bg-slate-400  p-6 text-center">
       <div>
-        <p className="text-white text-xl">Developed By Mustafa Khaled</p>
+        <a
+          className="text-white text-xl cursor-pointer"
+          href="https://github.com/MostafaKh1"
+        >
+          Developed By Mustafa Khaled
+        </a>
       </div>
     </footer>
   );
